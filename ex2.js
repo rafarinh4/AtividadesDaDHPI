86b442170d0a6412e02dc637b725cffc7901aaaf
rafarinh4 <rafaelcamargo089@gmail.com>
2021-08-16 22:08:09 -0300

    primeiro commit

---
+++ ex2.js
@@ -0,0 +1,9 @@
+let nome = "Rafael"
+let idade = 18
+let usuarioAtivo = false
+console.log('Nome: ')
+console.log(nome)
+console.log('Idade: ')
+console.log(idade)
+console.log('O usuário está ativo no sistema? ')
+console.log(usuarioAtivo)
