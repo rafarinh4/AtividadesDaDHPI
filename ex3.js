b63d5a583f49196eace526c92307bd50017534a5
rafarinh4 <rafaelcamargo089@gmail.com>
2021-08-16 22:47:54 -0300

    atividade 3

---
+++ ex3.js
@@ -0,0 +1,21 @@
+console.log(2+5)
+
+console.log(4-1)
+
+let a = 6
+let b = 4
+console.log(a+b)
+
+let c = a+b
+console.log(c)
+
+// se atribuir mais uma vez o let c mas colocar = "1" +1 dá um erro de sintax, então está como c = ...
+c = "1" +1
+console.log(c)
+
+c = "olá" + 23
+console.log(c)
+
+console.log(a+c)
+console.log(b-a)
+console.log(3*3)
